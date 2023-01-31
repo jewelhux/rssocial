049e3dfc201513186ad1d4ddfefa27b6f61d2db1
Jik789 <stix.jik@gmail.com>
2023-01-31 08:54:06 +0300

    feat: view all page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-// import DefaultStyle from './components/DefaultStyle';
-// import Footer from './components/Common/Footer';
-// import PageChat from './components/PageChat/PageChat';
-// import PageProfile from './components/PageProfile/PageProfile';
+import DefaultStyle from './components/DefaultStyle';
+import Footer from './components/Common/Footer';
+import PageChat from './components/PageChat/PageChat';
+import PageProfile from './components/PageProfile/PageProfile';
+import PageStart from './components/PageStart/PageStart';
 
 function App() {
   return (
     <>
-      {/* <DefaultStyle />
-      <PageStart />
-      <PageChat />
+      <DefaultStyle />
+      {/* <PageStart />
+      <PageChat /> */}
       <PageProfile />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
