a8895f8c14d9b8910abe7ee6e0dea8baf9088d4f
Jik789 <stix.jik@gmail.com>
2023-01-31 13:04:07 +0300

    refactor: header profile component

--- src/components/Common/HeaderComponent/HeaderComponentProfile.tsx
+++ src/components/Common/HeaderComponent/HeaderComponentProfile.tsx
@@ -1,20 +1,61 @@
-import React, { ReactElement } from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Badge, MenuItem, Menu} from '@mui/material';
+import React, { ReactElement, ReactEventHandler, useState } from 'react';
+import { AppBar, Box, Toolbar, IconButton, Typography, Badge, MenuItem, Menu } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import HeaderComponentMessage, { HeaderMails, HeaderAccount } from './HeaderComponentMessage';
+import MailIcon from '@mui/icons-material/Mail';
 
-function HeaderComponentProfile():ReactElement {
+// три точки вертикальные
+function HeaderComponentProfile(): ReactElement {
+
+  const [anchorElement, setAncrElement] = useState<null | HTMLElement>(null)
+  const [open, setOpen] = useState(false)
+
+  const handleClose = () => {
+    setAncrElement(null)
+    setOpen(false)
+  }
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAncrElement(event.currentTarget)
+    setOpen(true)
+  }
+
   return (
-    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-      <IconButton
-        size="large"
-        aria-label="show more"
-        aria-haspopup="true"
-        color="inherit"
+    <>
+      <Box sx={{ display: { xs: 'flex', md: 'none' } }}
       >
-        <MoreIcon />
-      </IconButton>
-    </Box>
+        <IconButton
+          onClick={handleOpen}
+          size="large"
+          aria-label="show more"
+          aria-haspopup="true"
+          color="inherit"
+
+        >
+          <MoreIcon />
+        </IconButton>
+      </Box>
+      <Menu
+        anchorEl={anchorElement}
+        open={open}
+        onClose={handleClose}
+        sx={{ display: { xs: 'flex', md: 'none', flexDirection: "column" } }}
+      >
+        <MenuItem >
+          <HeaderMails />
+          <Typography>
+            Почта
+          </Typography>
+        </MenuItem>
+        <MenuItem >
+          <HeaderAccount />
+          <Typography>
+            Аккаунт
+          </Typography>
+        </MenuItem>
+      </Menu>
+    </>
+
   );
 }
 
