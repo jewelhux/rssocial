632551c61ccdaf4cf1053394e8e8085c325e90e1
Jik789 <stix.jik@gmail.com>
2023-01-31 08:52:49 +0300

    feat: page profile components

---
+++ src/components/PageProfile/MainProfileComponent/ProfileComponentRight.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import { Typography } from '@mui/material';
+
+function ProfileComponentRight():ReactElement {
+  return (
+    <>
+      <Typography variant="h4" gutterBottom> Тут будет какая-то информация пользователя </Typography>
+    </>
+  );
+}
+
+export default ProfileComponentRight;
---
+++ src/components/PageProfile/PageProfile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Common/Header';
+import MainProfile from './MainProfile';
+
+function PageProfile() {
+  return (
+    <>
+      <Header />
+      <MainProfile /> 
+    </>
+  );
+}
+
+export default PageProfile;
