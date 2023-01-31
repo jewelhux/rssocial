2c9a0167047c09eaf093bcc33fb3eeb6fa92038e
Syderi <syderi@yahoo.com>
2023-01-31 13:28:34 +0300

    init: start rssocial

--- src/components/PageStart/HeaderLogin.tsx
+++ src/components/PageStart/HeaderLogin.tsx
@@ -1,17 +1,37 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Toolbar, AppBar, Container, IconButton, Typography, Box, Button } from '@mui/material';
 import { styled } from '@mui/material'
+import HeaderComponentSearch from '../Common/HeaderComponent/HeaderComponentSearch';
+import HeaderComponentMessage from '../Common/HeaderComponent/HeaderComponentMessage';
+import HeaderComponentProfile from '../Common/HeaderComponent/HeaderComponentProfile';
 
-function HeaderLogin():ReactElement {
+function HeaderLogin(): ReactElement {
+
+const [detailsHeaderComponentUser, setDetailsHeaderComponentUser ] = useState(true)
+const [detailsHeaderComponentSearch, setDetailsHeaderComponentSearch ] = useState(true)
+
   return (
     <AppBar position="static">
       <Container>
         <Toolbar>
-          <Typography variant='h5' sx={{ flexGrow: 1 }}>RSSocial</Typography>
+          <Box sx={{ display: 'flex', flexGrow: 1, alignItems: 'center' }}>
+            <Typography variant='h5' >RSSocial</Typography>
+            {detailsHeaderComponentSearch &&
+            <HeaderComponentSearch />
+            }
+          </Box>
+
           <Box sx={{ display: 'flex', columnGap: '10px' }}>
             <Button color='inherit' variant='outlined'>Вход</Button>
             <Button color='secondary' variant='contained'>Регистрация</Button>
           </Box>
+          {detailsHeaderComponentUser &&
+
+          <Box sx={{ display: 'flex', columnGap: '10px' }}>
+            <HeaderComponentMessage />
+            <HeaderComponentProfile />
+          </Box>
+          }
         </Toolbar>
       </Container>
     </AppBar>
