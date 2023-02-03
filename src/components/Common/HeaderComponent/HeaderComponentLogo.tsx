bee9d535b25f4b354e77fbda3c815efb6906a8ea
Jik789 <stix.jik@gmail.com>
2023-02-03 14:47:38 +0300

    feat: feed list

--- src/components/Common/HeaderComponent/HeaderComponentLogo.tsx
+++ src/components/Common/HeaderComponent/HeaderComponentLogo.tsx
@@ -5,8 +5,20 @@ import { Link } from 'react-router-dom';
 
 function HeaderComponentLogo():ReactElement {
   return (
-    <Button component={Link} color='inherit'  to="/" startIcon={<PublicIcon />}>
-      <Typography fontWeight={700} variant='h5'>RSSocial</Typography>
+    <Button 
+      component={Link} 
+      color='inherit'  
+      to="/" 
+      startIcon={<PublicIcon />} 
+      sx={{ padding: { xs: '0', md: '0 10px' }, minWidth: { xs: '20px', md: '64px' } }}
+    >
+        <Typography
+          fontWeight={700} 
+          variant='h5' 
+          sx={{ display: { xs: 'none', md: 'block'} }}
+        >
+          RSSocial
+        </Typography>
     </Button>
   );
 }
--- src/components/Pages/PageFeed/PageFeed.tsx
+++ src/components/Pages/PageFeed/PageFeed.tsx
@@ -1,12 +1,19 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import React from 'react';
 import FeedComponentFeed from './MainFeedComponent/FeedComponentFeed'
 
 function PageFeed() {
   return (
-    <Container sx={{ mb: 2, mt: 6, flexGrow: 1 }}>
-      <FeedComponentFeed />
+    <Container sx={{ mb: 2, mt: 6, flexGrow: 1, display: 'flex', flexDirection: 'column', gap: '15px', alignItems: 'center', textAlign: 'center' }}>
+      <Typography variant='h5'>Добро пожаловаьть в социальную сеть RSSocial</Typography>
+      <Typography>Последние посты пользователей:</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+        <FeedComponentFeed />
+        <FeedComponentFeed />
+        <FeedComponentFeed />
+        <FeedComponentFeed />
+      </Box>
     </Container>
   );
 }
