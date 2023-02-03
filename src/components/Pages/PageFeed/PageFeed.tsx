954e91ce28bec26199b00c3c9472afa94d90ed99
Jik789 <stix.jik@gmail.com>
2023-02-03 09:13:45 +0300

    feed first basic post

--- src/components/Pages/PageFeed/PageFeed.tsx
+++ src/components/Pages/PageFeed/PageFeed.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
 import { Container } from '@mui/system';
 import React from 'react';
+import FeedComponentFeed from './MainFeedComponent/FeedComponentFeed'
 
 function PageFeed() {
   return (
     <Container sx={{ mb: 2, mt: 6, flexGrow: 1 }}>
-      ТУТ ТИПО ЛЕНТА НОВОСТЕЙ
+      <FeedComponentFeed />
     </Container>
   );
 }
