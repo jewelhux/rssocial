c8c132ed49c395ddfbb4b8e301764fc22c718d1a
ferka123 <ferka911@gmail.com>
2023-02-18 11:50:13 +0200

    chore: backend initial setup

---
+++ backend/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      {
+        allowShortCircuit: true,
+      },
+    ],
+    "@typescript-eslint/array-type": [
+      "error",
+      {
+        default: "array",
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+};
